79eba6ac2141d110de8f1861f24f1eca8daa5124
OmarWhite <omar.white@target.com>
2020-01-10 15:16:34 -0600

    exports themes to playroom

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,2 +1,14 @@
-export {lightTheme} from './lightTheme'
-export {darkTheme} from './darkTheme'
+export {
+  base,
+  system,
+  funk,
+  future,
+  roboto,
+  dark,
+  deep,
+  swiss,
+  tosh,
+  bootstrap,
+  bulma,
+  tailwind
+} from "@theme-ui/presets";
